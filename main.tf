e11db85a950d37979c9c1fa77b6b138d15fbc5f9
ranjit71 <106545446+ranjit71@users.noreply.github.com>
2022-07-07 12:34:42 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  shared_credentials_file = "$/root/.aws/credentials"
+  shared_credentials_file = "/root/.aws/credentials"
   profile                 = "default"
   region                  = "us-east-1"
 }
