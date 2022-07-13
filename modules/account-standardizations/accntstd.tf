1494cd6d454c3a8e42934f55ade7a508bff755d7
ranjit71 <106545446+ranjit71@users.noreply.github.com>
2022-07-13 22:35:42 +0800

    Update accntstd.tf

--- modules/account-standardizations/accntstd.tf
+++ modules/account-standardizations/accntstd.tf
@@ -39,5 +39,5 @@ resource "aws_cloudformation_stack" "acc_std" {
     PermissionsBoundary = var.PermissionsBoundary
     
     }
- template_url = "https://${aws_s3_bucket.hitech_buck.id}.us-east-1.s3.amazonaws.com/account_std.yml"
+ template_url = "https://${aws_s3_bucket.hitech_buck.id}.s3.amazonaws.com/account_std.yml"
 }
