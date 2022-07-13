b92851202cb5f88b4a79f16667eeb8a549d10931
ranjit71 <106545446+ranjit71@users.noreply.github.com>
2022-07-13 13:00:24 +0800

    Update acc-sec.tf

--- modules/Account-Security/acc-sec.tf
+++ modules/Account-Security/acc-sec.tf
@@ -19,8 +19,11 @@ resource "aws_s3_bucket_object" "object" {
 # CLOUDFORMATION CREATION 
 
 resource "aws_cloudformation_stack" "acc-security" {
+  account_id     = "373108622786"
   depends_on = [aws_s3_bucket_object.object]
   name = "CFT-ACC-SECURITY"
   disable_rollback = true
   template_url = "https://${aws_s3_bucket.hitech_buck.id}-fips.us-east-1.s3.amazonaws.com/security.yml"
 }
+
+
